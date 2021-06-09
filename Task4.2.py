47272bff853c0a13fd6ed2a9ea027fd258a65359
priyansh7453 <68602017+priyansh7453@users.noreply.github.com>
2021-06-09 22:57:43 +0530

    Create Task4.2.py

---
+++ Task4.2.py
@@ -0,0 +1,18 @@
+j=450
+for i in range(450,500):
+  if j>=400:
+    photo[i,j] = [255,255,255]
+   j=j-1
+  
+  j=450
+  for i in range(450,500):
+  if j<=500:
+    photo[i,j] = [255,255,255]
+   j=j+1
+  
+  j=650
+for i in range(450,500):
+  if j<=400:
+    photo[i,j] = [255,255,255]
+   j=j+1
+  
